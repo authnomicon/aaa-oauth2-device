ce78aaa0b0626a671d7cc356cd46fa08346eef9b
Jared Hanson <jaredhanson@gmail.com>
2017-10-24 12:35:53 -0700

    Export device authorization service.

--- app/http/service.js
+++ app/http/service.js
@@ -2,12 +2,12 @@ exports = module.exports = function(createHandler) {
   var express = require('express');
   var router = new express.Router();
   
-  router.post('/init', createHandler);
+  router.post('/', createHandler);
   
   return router;
 };
 
-exports['@implements'] = 'http://schemas.authnomicon.org/js/oauth2/http/DeviceService';
+exports['@implements'] = 'http://schemas.authnomicon.org/js/oauth2/http/DeviceAuthorizationService';
 exports['@require'] = [
   './handlers/device'
 ];
