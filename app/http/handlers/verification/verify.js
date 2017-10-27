bd2d1cb628bdcf810d8366c0924260e83bc53889
Jared Hanson <jaredhanson@gmail.com>
2017-10-26 17:39:22 -0700

    Stub out transaction loading.

--- app/http/handlers/verification/verify.js
+++ app/http/handlers/verification/verify.js
@@ -1,4 +1,4 @@
-exports = module.exports = function(parse) {
+exports = module.exports = function(server, loadTransaction, processTransaction, completeTransaction, prompt, parse) {
 
   function verify(req, res, next) {
     console.log('VERIFY IT')
@@ -7,11 +7,21 @@ exports = module.exports = function(parse) {
 
 
   return [
-    parse(),
-    verify
+    parse('application/x-www-form-urlencoded'),
+    server.resume(
+      loadTransaction,
+      processTransaction,
+      completeTransaction
+    ),
+    prompt()
   ];
 };
 
 exports['@require'] = [
+  'http://schemas.authnomicon.org/js/oauth2/Server',
+  '../../middleware/loadtransaction',
+  'http://schemas.authnomicon.org/js/oauth2/http/authorize/processTransactionFunc',
+  'http://schemas.authnomicon.org/js/oauth2/http/authorize/completeTransactionFunc',
+  'http://schemas.authnomicon.org/js/oauth2/http/middleware/prompt',
   'http://i.bixbyjs.org/http/middleware/parse'
 ];
---
+++ app/http/middleware/loadtransaction.js
@@ -0,0 +1,31 @@
+exports = module.exports = function() {
+  
+  return function loadTransaction(req, res, next) {
+    console.log('CUSTOM LOAD TRANSACTION!');
+    console.log(req.body)
+    
+    req.oauth2 = { transactionID: '1235',
+      client: 
+       { id: '1',
+         name: 'OAuth 1.0 Client',
+         redirectURIs: [ 'http://127.0.0.1:3000/return' ],
+         tokenEndpointAuthMethod: 'client_secret_basic',
+         grantTypes: [ 'authorization_code' ],
+         responseTypes: [ 'code' ],
+         scope: [ 'send', 'receive' ],
+         authenticationSchemes: [ 'bearer', 'hawk' ] },
+      req: 
+      { type: 'code',
+         //type: 'device_code',
+         display: 'page' },
+      user: undefined };
+    
+    req.state = req.oauth2;
+    req.state.handle = req.oauth2.transactionID;
+    
+    next();
+    
+  };
+};
+
+exports['@require'] = [];
